757002611baa320ad4e40cab1ee79cb91b7a225e
mmurrs <mmurrayc@umich.edu>
2024-02-16 19:28:51 -0800

    vercel f0

--- app/api/cron/auth.ts
+++ app/api/cron/auth.ts
@@ -1,34 +1,34 @@
-export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+// export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
-export function GET(request: Request) {
-  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
-}
-
-// async function authenticateSpotify() {
-//   const url = 'https://accounts.spotify.com/api/token';
-//   const headers = {
-//     'Authorization': 'Basic ' + btoa(process.env.client_id + ':' + process.env.client_secret),
-//     'Content-Type': 'application/x-www-form-urlencoded'
-//   };
-
-//   const body = new URLSearchParams();
-//   body.append('grant_type', 'client_credentials');
-//   try {
-//     const response = await fetch(url, {
-//       method: 'POST',
-//       headers: headers,
-//       body: body,
-//     });
-
-//     if(!response.ok) {
-//       throw new Error(`Error in Auth response: ${response.statusText}`);
-//     }
-
-//     const data = await response.json();
-//     token = data.access_token;
-//     console.log("Token", token);
-//     console.log("Toklen expires in: data.expires_in");
-//     } catch(error) {
-//       console.error('Error ', error);
-//     }
+// export function GET(request: Request) {
+//   return new Response(`Hello from ${process.env.VERCEL_REGION}`);
 // }
+
+// // async function authenticateSpotify() {
+// //   const url = 'https://accounts.spotify.com/api/token';
+// //   const headers = {
+// //     'Authorization': 'Basic ' + btoa(process.env.client_id + ':' + process.env.client_secret),
+// //     'Content-Type': 'application/x-www-form-urlencoded'
+// //   };
+
+// //   const body = new URLSearchParams();
+// //   body.append('grant_type', 'client_credentials');
+// //   try {
+// //     const response = await fetch(url, {
+// //       method: 'POST',
+// //       headers: headers,
+// //       body: body,
+// //     });
+
+// //     if(!response.ok) {
+// //       throw new Error(`Error in Auth response: ${response.statusText}`);
+// //     }
+
+// //     const data = await response.json();
+// //     token = data.access_token;
+// //     console.log("Token", token);
+// //     console.log("Toklen expires in: data.expires_in");
+// //     } catch(error) {
+// //       console.error('Error ', error);
+// //     }
+// // }
