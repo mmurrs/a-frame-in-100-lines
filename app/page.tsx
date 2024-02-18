27b0ec4c4b204bd51d792a09d145201e076db43c
mmurrs <mmurrayc@umich.edu>
2024-02-18 12:42:26 -0800

    updated image

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const frameMetadata = getFrameMetadata({
       label: 'Start the groove',
     },
   ],
-  image: `${process.env.NEXT_PUBLIC_URL}ying_yang_mid.png`,
+  image: `${process.env.NEXT_PUBLIC_URL}dj_arch.png`,
   post_url: `${process.env.NEXT_PUBLIC_URL}api/frame`,
 });
 
@@ -15,9 +15,9 @@ export const metadata: Metadata = {
   title: 'Spotify Testing',
   description: 'LFG',
   openGraph: {
-    title: 'mane to main',
+    title: 'Groove Provider',
     description: 'LFG',
-    images: [`${process.env.NEXT_PUBLIC_URL}ying_yang_mid.png`],
+    images: [`${process.env.NEXT_PUBLIC_URL}dj_arch.png`],
   },
   other: {
     ...frameMetadata,
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
-      <h1>mane to main</h1>
+      <h1>Groove Provider</h1>
     </>
   );
 }
