d3af8ae08cc07fe6b301b539fcb46211f4665d95
mmurrs <mmurrayc@umich.edu>
2024-02-17 18:12:07 -0800

    updated schedule

--- app/api/cron/auth.ts
+++ app/api/cron/auth.ts
@@ -1,5 +1,5 @@
-// export const dynamic = 'force-dynamic'; // static by default, unless reading the request
- 
+export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+
 export function GET(request: Request) {
     process.env.TEST = "nope"
   return new Response(`Hello from ${process.env.TEST}`);
