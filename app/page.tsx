023fdbb3246ef4b6ee810660b94d4b64555f61ce
mmurrs <mmurrayc@umich.edu>
2024-02-21 15:51:55 -0700

    tried diff aspect ratio

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const frameMetadata = getFrameMetadata({
       label: 'Start the groove',
     },
   ],
-  image: `${process.env.NEXT_PUBLIC_URL}dj_arch.png`,
+  image: `${process.env.NEXT_PUBLIC_URL}dj_arch_1.png`,
   post_url: `${process.env.NEXT_PUBLIC_URL}api/frame`,
 });
 
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'Groove Provider',
     description: 'LFG',
-    images: [`${process.env.NEXT_PUBLIC_URL}dj_arch.png`],
+    images: [`${process.env.NEXT_PUBLIC_URL}dj_arch_1.png`],
   },
   other: {
     ...frameMetadata,
